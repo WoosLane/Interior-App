0c50ec9f10197563f6cc0cc63803adf35cb41f48
Yoonwoo Lee <yoonwoo99@gmail.com>
2022-05-19 11:18:16 +0900

    홈 화면 추가

---
+++ lib/screens/home/components/kurly_recommend_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KurlyRecommendPage extends StatelessWidget {
+  const KurlyRecommendPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
